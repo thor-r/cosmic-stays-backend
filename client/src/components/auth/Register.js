5c295e55e499fb83630a21ea3f54d7c20159e8ce
Neil Mcfayden <neilmcfayden@Neils-MacBook-Pro.local>
2022-02-12 14:15:52 +0000

    login and register pages

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import axios from 'axios' 
+
+// useNavigate allows us to redirect users to different routes the React way (no reload)
+import { useNavigate } from 'react-router-dom'
+
+// Import Bootstrap Components
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const Register = () => {
+
+  // navigate
+  const navigate = useNavigate()
+
+  const [ formData, setFormData ] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  })
+
+  const [ formErrors, setFormErrors ] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  })
+
+  const handleChange = (e) => {
+    const newObj = { ...formData, [e.target.name]: e.target.value }
+    setFormData(newObj)
+    setFormErrors({ ...formErrors, [e.target.name]: '' })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault() // this prevents reload
+    try {
+      await axios.post('/api/login', formData)
+      navigate('/login')
+    } catch (err) {
+      setFormErrors(err.response.data.errors)
+    }
+  }
+
+  return (
+    <div className="form-page">
+      <Container>
+        <Form onSubmit={handleSubmit} className='mt-4'>
+          <h2>Register</h2>
+          {/* Username */}
+          <Form.Group className='mb-2'>
+            <Form.Label htmlFor='username'>Username</Form.Label>
+            <Form.Control onChange={handleChange} type="text" name="username" placeholder="Username" defaultValue={formData.username} />
+            {formErrors.username && <Form.Text>{formErrors.username}</Form.Text>}
+          </Form.Group>
+          {/* Email */}
+          <Form.Group className='mb-2'>
+            <Form.Label htmlFor="email">Email Address</Form.Label>
+            <Form.Control onChange={handleChange} type="email" name="email" placeholder="Email" defaultValue={formData.email} />
+            {formErrors.email && <Form.Text>{formErrors.email}</Form.Text>}
+          </Form.Group>
+          {/* Password */}
+          <Form.Group className='mb-2'>
+            <Form.Label htmlFor="password">Password</Form.Label>
+            <Form.Control onChange={handleChange} type="password" name="password" placeholder="Password" defaultValue={formData.password} />
+            {formErrors.password && <Form.Text>{formErrors.password}</Form.Text>}
+          </Form.Group>
+          {/* Password Confirmation */}
+          <Form.Group className='mb-2'>
+            <Form.Label htmlFor="passwordConfirmation">Confirm Password</Form.Label>
+            <Form.Control onChange={handleChange} type="password" name="passwordConfirmation" placeholder="Confirm Password" defaultValue={formData.passwordConfirmation} />
+            {formErrors.passwordConfirmation && <Form.Text>{formErrors.passwordConfirmation}</Form.Text>}
+          </Form.Group>
+          {/* Submit */}
+          <Form.Group className='text-center mt-4'>
+            <Button variant="warning" type="submit">Submit</Button>
+          </Form.Group>
+        </Form>
+      </Container>
+    </div>
+  )
+}
+
+export default Register
